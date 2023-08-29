6d24e0db12226c7df1f465b3397babfa5fe57493
Anastasiia Ivanova <anastasiia1ivanova1@gmail.com>
2023-08-29 23:21:09 +0000

    done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const sentence = (who, action, what, when);
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let randomIdx = Math.floor(Math.random() * who.length);
+  let whoR = who[randomIdx];
+  let actionR = action[Math.floor(Math.random() * action.length)];
+  let whatR = what[Math.floor(Math.random() * what.length)];
+  let whenR = when[Math.floor(Math.random() * when.length)];
+  const resulMessage = whoR + " " + actionR + " " + whatR + " " + whenR;
+  document.getElementById("excuse").innerHTML = resulMessage;
 };
